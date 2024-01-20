03c9c74d6f679b3327cbd665ca14d1629e2f8f85
harshshrivastava16 <126142079+harshshrivastava16@users.noreply.github.com>
2024-01-20 23:08:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+var loader = document.querySelector("#loader")
+setTimeout(function(){
+    loader.style.top = "-100%"
+},4000)
